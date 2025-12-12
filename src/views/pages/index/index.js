49f43f385c3ae20a2e94ffeb114dc25f533b0330
TasyDev <tamayocontat@gmail.com>
2025-12-11 22:59:16 -0500

    Add: Dom calls

--- src/views/pages/index/index.js
+++ src/views/pages/index/index.js
@@ -1,11 +1,21 @@
 import { getData } from "../../../data/api/getAll.js";
 
+const dom = document.getElementById("Try")
+
 async function tryGetData() {
     let saveData = [];
     try {
-        saveData = await getData();
-        console.log(saveData);
+        const saveData = await getData();
+        dom.innerHTML = html(saveData);
     } catch (error) {
         console.log(error);
     }
 }
+
+function html(data) {
+    return data.map(item => {
+        return `<img src="${item.images}" alt="${item.slug}"/> <h1>${item.title}</h1>`;
+    }).join('');
+}
+
+tryGetData()
