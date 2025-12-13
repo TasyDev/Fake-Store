1adfb568de4c806a5347fcd45e7711cf64a6bd95
TasyDev <tamayocontat@gmail.com>
2025-12-12 19:30:51 -0500

    Add: search call

--- src/data/api/getByID.js
+++ src/data/api/getByID.js
@@ -1,5 +1,11 @@
-export async function getDataById(id) {
-    const res = await fetch(`https://api.escuelajs.co/api/v1/products${id}`);
+export async function getDataById(slug) {
+    const res = await fetch(`https://api.escuelajs.co/api/v1/products/slug/${slug}`);
+    if (!res.ok) throw new Error("Request failed");
+    return await res.json();
+}
+
+export async function getDataBySearch(title) {
+    const res = await fetch(`https://api.escuelajs.co/api/v1/products/?title=${title}`);
     if (!res.ok) throw new Error("Request failed");
     return await res.json();
 }
