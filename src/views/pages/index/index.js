1167165394f6f29965e900ce30b7ec45ebacd666
TasyDev <tamayocontat@gmail.com>
2025-12-13 22:31:53 -0500

    Refactor: Enhance category loading and display in index.js

--- src/views/pages/index/index.js
+++ src/views/pages/index/index.js
@@ -7,21 +7,44 @@ const categorySection = document.createElement("section");
 const lastProductsSection = document.createElement("section");
 
 //HTML inyection
-categorySection.className = "row justify-content-center h-100 w-100 py-5";
+categorySection.className = "row justify-content-center g-3 h-100 w-100 py-5 px-5";
 heroSectionHTML.after(categorySection);  
 lastProductsSection.className = "last-products";
 categorySection.after(lastProductsSection);
 
 function mensaggeWaiting() {
-    const wating = '<h2 class="text-white">Cargando</h2>'
-    lastProductsSection.innerHTML = wating
-    categorySection.innerHTML = wating
+    const loadingMessage = document.createElement("h2");
+    loadingMessage.className = "text-white text-center w-100";
+    loadingMessage.textContent = "Cargando...";
+    categorySection.appendChild(loadingMessage);
 }
 
 async function getCategorys() {
     mensaggeWaiting()
     const categoryArray = await categories();
-    categorySection.innerHTML = "Hola"
+    const first6Categories = categoryArray.slice(0, 6);
+    printCategory(first6Categories);
+}
+
+function printCategory(data){
+    const loadingMessage = categorySection.querySelector('h2');
+    loadingMessage.remove();
+    
+    data.map((i) =>{
+        const divCategory = document.createElement("div");
+        divCategory.className = "col-md-4 pb-3";
+        divCategory.innerHTML = `
+            <div class="black-background rounded-4 p-4 h-100">
+                <div class="d-flex justify-content-between align-items-start h-100">
+                    <div class="flex-grow-1 p-3">
+                        <h2 class="h2 text-white mb-3">${i.name}</h2>
+                    </div>
+                    <img src=${i.image} alt=${i.name} class="flex-shrink-0 rounded-3" style="width:150px; height:150px; object-fit:cover;">
+                </div>
+            </div>
+        `;
+        categorySection.appendChild(divCategory);
+    })
 }
 
 getCategorys();
