ad07df4e5abb962c3f0ec1da58ff7b0c3c536c8d
TasyDev <tamayocontat@gmail.com>
2025-12-13 19:24:57 -0500

    Refactor: Update index.html structure and enhance index.js for dynamic section creation

--- src/data/api/getCategory.js
+++ src/data/api/getCategory.js
@@ -1,5 +1,11 @@
+/* How to call
+- await categories()
+
+*/
+
 export async function categories() {
     const res = await fetch("https://api.escuelajs.co/api/v1/categories");
     if (!res.ok) throw new Error("Request failed");
     return await res.json();
 }
+
--- src/views/pages/index/index.js
+++ src/views/pages/index/index.js
@@ -1,57 +1,16 @@
-import { getData } from "../../../data/api/getAll.js";
 import { categories } from "../../../data/api/getCategory.js";
 
-const categorysDom = document.getElementById("categoryCards")
+const heroSectionHTML = document.getElementById("hero");
 
-async function tryGetData() {
+//Gobal variables
+const categorySection = document.createElement("section");
+const lastProductsSection = document.createElement("section");
 
-    function html(data) {
-        return data.map(item => {
-            return `<img src="${item.images}" alt="${item.slug}"/> <h1>${item.title}</h1>`;
-        }).join('');
-    }
+//HTML inyection
+categorySection.className = "row justify-content-center h-100 w-100 py-5";
+heroSectionHTML.after(categorySection);  
+lastProductsSection.className = "last-products";
+categorySection.after(lastProductsSection);
 
-    let saveData = [];
-    try {
-        const saveData = await getData();
-        dom.innerHTML = html(saveData);
-    } catch (error) {
-        console.log(error);
-    }
-}
 
-async function tryGetCategory() {
-    try {
-        const saveData = await categories();
-        const sliceData = saveData.slice(0, 6);
 
-        let rowsHtml = '';
-        for (let i = 0; i < sliceData.length; i += 3) {
-            const cards = sliceData.slice(i, i + 3).map(item => {
-                return `
-                <div class="col-md-4 pb-3">
-                    <div class="black-background rounded-4 p-4 h-100">
-                        <div class="d-flex justify-content-between align-items-start h-100">
-                            <div class="flex-grow-1 p-3">
-                                <h2 class="h2 text-white mb-3">${item.name}</h2>
-                            </div>
-                            <img src="${item.image}" alt="${item.slug}" class="flex-shrink-0 rounded-3" style="width:150px; height:150px; object-fit:cover;">
-                        </div>
-                    </div>
-                </div>
-                `;
-            }).join('');
-            rowsHtml += `<div class="row justify-content-center g-3">${cards}</div>`;
-        }
-
-        categorysDom.innerHTML = `<div class="col-md-10 p-5 rounded-4">${rowsHtml}</div>`;
-    } catch (error) {
-        console.log(error);
-    }
-
-    
-}
-
-document.addEventListener("DOMContentLoaded", () => {
-    tryGetCategory();
-});
