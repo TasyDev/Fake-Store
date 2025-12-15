d89a11e409941c5697b083acb93a7ab2524307f7
TasyDev <tamayocontat@gmail.com>
2025-12-15 10:57:16 -0500

    Enhance: Implement last products section in index.js and remove unused HTML in index.html

--- src/views/pages/index/index.js
+++ src/views/pages/index/index.js
@@ -10,7 +10,8 @@ const lastProductsSection = document.createElement("section");
 //HTML inyection
 categorySection.className = "row justify-content-center g-3 h-100 w-100 py-5 px-5";
 heroSectionHTML.after(categorySection);  
-lastProductsSection.className = "last-products";
+// Sección de últimos productos con padding horizontal para que no quede pegada a los bordes
+lastProductsSection.className = "row justify-content-center w-100 py-5 px-4 last-products";
 categorySection.after(lastProductsSection);
 
 function createMensaggeWaiting(idSection) {
@@ -20,9 +21,11 @@ function createMensaggeWaiting(idSection) {
     idSection.appendChild(createLoadingMessage);
 }
 
-function deleteMensaggeWaiting() {
-    const loadingMessage = categorySection.querySelector("h2");
-    loadingMessage.remove();
+function deleteMensaggeWaiting(idSection) {
+    const loadingMessage = idSection.querySelector("h2");
+    if (loadingMessage) {
+        loadingMessage.remove();
+    }
 }
 
 async function getCategorys() {
@@ -33,7 +36,7 @@ async function getCategorys() {
 
 
 function printCategory(data){
-    deleteMensaggeWaiting()
+    deleteMensaggeWaiting(categorySection)
     data.map((i) =>{
         const divCategory = document.createElement("div");
         divCategory.className = "col-md-4 pb-3";
@@ -50,6 +53,43 @@ function printCategory(data){
         categorySection.appendChild(divCategory);
     })
 }
+
+async function get3Products() {
+    createMensaggeWaiting(lastProductsSection)
+    const productsArray = await getData();
+    printProduts(productsArray);
+}
+
+function printProduts(data){
+    deleteMensaggeWaiting(lastProductsSection);
+    const row = document.createElement("div");
+    row.className = "row row-cols-1 row-cols-md-2 row-cols-lg-3 g-4 col-12 col-md-10";
+    data.map((i) => {
+        const divProduct = document.createElement("div");
+        divProduct.className = "col d-flex";
+        divProduct.innerHTML = `
+            <div class="black-background p-4 p-md-5 rounded-4 w-100 h-100">
+                <img src="${i.images}" alt="${i.title}" class="img-fluid w-100 pb-2">
+                <h3 class="text-white">
+                    ${i.title}
+                </h3> 
+                <p class="text-white">${i.description}</p>
+                <div class="d-flex justify-content-between align-items-center mt-3">
+                    <a class="text-white">Comprar ahora</a>
+                    <a>
+                        <img src="src/assets/icons/bag-add.svg" alt="Agregar al carrito" class="green-background rounded-3 px-3" style="width: 65px;">
+                    </a>
+                </div>
+            </div>
+        `;
+        row.appendChild(divProduct);
+    });
+
+    lastProductsSection.appendChild(row);
+}
+
+get3Products()
+
 getCategorys();
 
 
