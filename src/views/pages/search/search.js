6d5b3f8065ed0ddf107e809d6d8954d463485418
TasyDev <tamayocontat@gmail.com>
2025-12-19 16:44:58 -0500

    feat: add search page logic to retrieve data using URL query parameter

--- src/views/pages/search/search.js
+++ src/views/pages/search/search.js
@@ -1,1 +1,15 @@
+import { getDataBySearch } from "../../../data/api/getByID.js";
+
+function getTitleSearch() {
+    const params = new URLSearchParams(window.location.search);
+    return params.get("q");
+}
+
+const title = getTitleSearch();
+
+if (title) {
+    getDataBySearch(title);
+} else {
+    console.error("No hay title en la URL");
+}
 
