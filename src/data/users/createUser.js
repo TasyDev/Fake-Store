eb6c9c6395a2a1e0c8328b50a9327378f2722d17
TasyDev <tamayocontat@gmail.com>
2026-01-06 19:06:50 -0500

    Add createUser function for user creation API
    Introduces createUser.js with an async function to create users via the escuelajs API. Handles API request, error logging, and returns user data on success.

---
+++ src/data/users/createUser.js
@@ -0,0 +1,27 @@
+export async function createUser(name, email, password) {
+    try {
+        await newUser(name, email, password)
+    } catch (err) {
+        console.error('Request failed:', err.message)
+
+    }
+}
+
+async function newUser(name, email, password) {
+    const res = await fetch('https://api.escuelajs.co/api/v1/users/', {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({
+            name,
+            email,
+            password,
+            avatar: 'https://picsum.photos/800'
+        })
+    })
+    const data = await res.json()
+    if (!res.ok) {
+        console.error(data)
+        throw new Error('Error creating user')
+    }
+    return data
+}
